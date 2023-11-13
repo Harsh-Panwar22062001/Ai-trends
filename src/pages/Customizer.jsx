2d2bb7b726ea2490d80025d118c20259b432c6af
Harsh Panwar <20001003044harsh@dcrustm.org>
2023-11-13 14:03:11 +0530

    tabcontent-done

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -5,12 +5,45 @@ import config from '../config/config';
 import state from '../store';
 import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
-import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'; // Fixed the import path
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'; 
 import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => { // Corrected component name from "Customizer" to "Customizer"
     const snap = useSnapshot(state);
+    const [file , setFile] = useState('');
+
+const [prompt , setPrompt ] = useState('');
+
+const [generatingImg , setGeneratingImg] =useState(false);
+
+const [activeEditorTab , setActiveEditorTab] = useState("");
+
+const[activeFilterTab , setActiveFilterTab] = useState({
+    logoShirt : true ,
+    stylishShirt : false,
+});
+
+
+
+// show tab content depending in the activeTab
+
+const generateTabContent =() => {
+
+
+    switch(activeEditorTab){
+        case "colorpicker":
+            return <ColorPicker/>
+            case "filepicker":
+            return <FilePicker/>
+            case "aipicker":
+            return <AIPicker/>
+            default:
+            return null;
+    }
+
+}
+
     return (
 
         
@@ -30,10 +63,12 @@ const Customizer = () => { // Corrected component name from "Customizer" to "Cus
                             <Tab
                             key={tab.name}
                             tab = {tab}
-                            handleClick ={()=>
-                            {}}
+                            handleClick ={()=> setActiveEditorTab(tab.name)}
+                            
                             />
                         ))}
+
+                        {generateTabContent()}
                      </div>   
                     </div>
                     
